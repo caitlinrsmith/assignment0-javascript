23be44e65b24e7386c9e4db7c1abe023492b58e3
caitlinrsmith <catie12smith@gmail.com>
2024-03-25 15:38:53 -0500

    testing autograding

--- hello.js
+++ hello.js
@@ -1,6 +1,23 @@
-function hello() {
-	// TODO: change this string so that your program prints "Hello world!" 
-	return "Hello world!";
-}
+// function hello() {
+// 	// TODO: change this string so that your program prints "Hello world!" 
+// 	return "Hello world!";
+// }
  
-module.exports = hello;
+// module.exports = hello;
+
+// console.log(Boolean("true"));
+// console.log(Boolean("TRUE"));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(''));
+// console.log(Boolean('LaunchCode'));
+// console.log(4 == "4");
+
+let num = 7;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
+console.log("hello");
